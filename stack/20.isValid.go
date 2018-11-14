5ca6d81d94b3808dc5676a5aec79f0cf0b3e8f67
zhangjun <jun.zhang@yunshipei.com>
2018-11-14 19:42:34 +0800

    isValid solution2

--- stack/20.isValid.go
+++ stack/20.isValid.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+var hashMapReverse = map[string]string{")": "(", "}": "{", "]": "["}
+var hashMap = map[string]string{"(": ")", "{": "}", "[": "]"}
+
 /**
  * 1.Traversal string for _, v := range s string(v)
  * 2.define slice := make([]string, 0) not length len(s)
@@ -9,14 +12,6 @@ import "fmt"
  * 4.err: out of indexs
  */
 func isValid1(s string) bool {
-	hashMap := make(map[string]string, 3)
-	hashMapReverse := make(map[string]string, 3)
-	hashMap["("] = ")"
-	hashMap["{"] = "}"
-	hashMap["["] = "]"
-	hashMapReverse[")"] = "("
-	hashMapReverse["}"] = "{"
-	hashMapReverse["]"] = "["
 	var result bool = false
 	slice := make([]string, 0)
 	for _, v := range s {
@@ -38,6 +33,66 @@ func isValid1(s string) bool {
 	return result
 }
 
+/**
+ * 1.generator stack
+ * 2.stack => previous next data
+ */
+type Node struct {
+	data     string
+	next     *Node
+	previous *Node
+}
+
+// current pointer
+type Stack struct {
+	head *Node
+}
+
+func NewStack() *Stack {
+	return &Stack{head: &Node{}}
+}
+
+func (s *Stack) push(data string) {
+	node := &Node{data: data, next: nil, previous: s.head}
+	s.head.next = node
+	s.head = node
+}
+
+func (s *Stack) pop() (string, bool) {
+	n := s.head
+	if s.head == nil || s.head.previous == nil {
+		return "", false
+	}
+	data := n.data
+	s.head.previous.next = nil
+	s.head = s.head.previous
+	return data, true
+}
+
+func isValid2(s string) bool {
+	var result bool = false
+	stack := NewStack()
+
+	for _, v := range s {
+		value := string(v)
+		if _, ok := hashMap[value]; ok {
+			stack.push(value)
+		} else if v1, ok := hashMapReverse[value]; ok {
+			//if first elem is hashMapReverse then return false
+			//judge from hashMapReverse[value] pair
+			if stack.head.previous != nil && stack.head.data == v1 {
+				stack.pop()
+			} else {
+				return result
+			}
+		}
+	}
+	if stack.head.previous == nil {
+		result = true
+	}
+	return result
+}
+
 func main() {
-	fmt.Println(isValid1("]"))
+	fmt.Println(isValid2("[[{}]]"))
 }
